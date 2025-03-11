c86de1dbbce7d2ec09f2df8017ab67c7e3987f92
abdur.raheem <abdur.raheem@devways.digital>
2025-03-12 01:30:17 +0400

    add: fetch status api url

--- apps/apis/urls.py
+++ apps/apis/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import JobListCreateView
+from .views import JobListCreateView, JobStatusAPIView
 
 urlpatterns = [
     path('jobs/', JobListCreateView.as_view(), name='job-list'),
+    path('jobs/<int:pk>/', JobStatusAPIView.as_view(), name='job-status'),
 ]
